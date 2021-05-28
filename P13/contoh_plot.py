e089524cf354ae97a44f2c6c271e9f6788fad0e8
Wimbuh Tri <55055182+wimbuhTri@users.noreply.github.com>
2021-05-28 08:53:54 +0700

    update

--- P13/contoh_plot.py
+++ P13/contoh_plot.py
@@ -13,11 +13,25 @@ def scatter():
 	plt.scatter(x,cos_x)
 	plt.show()
 
-def bar():
+def bar():	
+	x = np.arange(5)
+	"""
+	y = np.linspace(0,3,5)
+	labels = ["toddler","childhood","teens","mature","old"]
 	fig, ax = plt.subplots()
-	x = np.arange(50)
-	y = np.arange(50)
-	plt.bar(y,x)
+	ax.bar(y,x)
+	ax.set_ylabel('Potential')
+	ax.set_xlabel('Stages')
+	ax.set_title('Linear Gibberish Progession')
+	#ax.set_xticks(x)
+	ax.set_xticklabels(labels)
+	#ax.legend()
+	"""
+	fig = plt.figure()
+	ax = fig.add_axes([0,0,1,1])
+	langs = ['C', 'C++', 'Java', 'Python', 'PHP']
+	students = [23,17,35,29,12]
+	ax.bar(langs,x)
 	plt.show()
 
-	
+bar()
