4b70b7f5ffc90b315812a1aba5f8b8ec14dae406
Wimbuh Tri <55055182+wimbuhTri@users.noreply.github.com>
2021-05-28 09:35:53 +0700

    Create advance_histogram.py

---
+++ P13/advance_histogram.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = np.random.randint(20, 50, 25)
+#data = read_using_numpy()
+grid_ID = np.arange(25)
+
+fig, ax = plt.subplots()
+ax.bar(grid_ID, data, width=0.8, align='center')
+ax.set(xticks=range(grid_ID.size)) #label tick pada x axis
+ax.set_ylabel('Potential')
+ax.set_xlabel('Stages')
+ax.set_title('Noise Progession')
+plt.show()
+
+"""starter code by Joe Kington Posted on stackOverflow 
+https://stackoverflow.com/questions/27083051/matplotlib-xticks-not-lining-up-with-histogram"""
